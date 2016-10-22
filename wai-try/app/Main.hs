c0d9311b3b630a5feb8987cad28a75bb57870fa8
Alexander Lobov <alexanderlobov@yandex.ru>
2016-10-23 00:22:19 +0300

    Explore wai package

---
+++ wai-try/app/Main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.RequestLogger (logStdout)
+
+app :: Application
+app request respond = respond $ case rawPathInfo request of
+    "/" -> index
+    "/raw/" -> plainIndex
+    _ -> notFound
+
+pathToIndex = "/home/alex/src/haskell/wai-try/app/index.html"
+
+index :: Response
+index = responseFile
+    status200
+    [("Content-Type", "text/html")]
+    pathToIndex
+    Nothing
+
+plainIndex :: Response
+plainIndex = responseFile
+    status200
+    [("Content-Type", "text/plain")]
+    pathToIndex
+    Nothing
+
+notFound :: Response
+notFound = responseLBS
+    status404
+    [("Content-Type", "text/plain")]
+    "404 - Not Found"
+
+main :: IO ()
+main = do
+    putStrLn $ "http://localhost:8080/"
+    run 8080 (logStdout app)
