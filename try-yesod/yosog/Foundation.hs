7319ccb96dc66a677ead975a0c37f8faeddcbf17
Alexander Lobov <alexanderlobov@yandex.ru>
2016-11-03 00:41:04 +0300

    Added blog handler

--- try-yesod/yosog/Foundation.hs
+++ try-yesod/yosog/Foundation.hs
@@ -142,6 +142,8 @@ instance Yesod App where
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
+    isAuthorized BlogR _ = return Authorized
+    isAuthorized (ArticleR _) _ = return Authorized
 
     isAuthorized ProfileR _ = isAuthenticated
 
---
+++ try-yesod/yosog/Handler/Blog.hs
@@ -0,0 +1,25 @@
+module Handler.Blog
+    ( getBlogR
+    , postBlogR
+    )
+where
+
+import Import
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+
+instance YesodNic App
+
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+    <$> areq textField "Title" Nothing
+    <*> areq nicHtmlField "Content" Nothing
+
+getBlogR :: Handler Html
+getBlogR = do
+    articles <- runDB $ selectList [] [Desc ArticleTitle]
+    (articleWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        $(widgetFile "articles")
+
+postBlogR :: Handler Html
+postBlogR = error "Not yet implemented: postBlogR"
