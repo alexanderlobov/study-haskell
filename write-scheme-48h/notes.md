318e289a580b4b4c2e1ed8d73599cbb3297d5da1
Alexander Lobov <alexanderlobov@yandex.ru>
2017-02-16 11:02:56 +0300

    Stub for HUnit tests

--- write-scheme-48h/notes.md
+++ write-scheme-48h/notes.md
@@ -1,4 +1,4 @@
 Todo:
 
 - tests
-- build
+- ex 6 in [Parsing](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours/Parsing)
--- write-scheme-48h/src/Test.hs
+++ write-scheme-48h/src/Test.hs
@@ -1,5 +1,10 @@
 import Scheme
-import System.Exit
+import Test.HUnit
 
-main :: IO ()
-main = exitFailure
+test1 = TestCase (assertEqual "first test" 1 2)
+test2 = TestCase (assertEqual "second test" 1 1)
+test3 = TestCase $ 1 @=? 3
+
+tests = TestList [test1, test2, test3]
+
+main = runTestTT tests
